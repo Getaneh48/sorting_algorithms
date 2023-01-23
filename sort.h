31965b5b759b4e98c165ec4218c62fc6182a7775
Getaneh48 <getaneh11@gmail.com>
2023-01-23 10:13:14 +0300

    betty fixed

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,8 +11,13 @@ void print_list(const listint_t *list)
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+
 #endif
