ef1c59008d13dada9178c75b0422e2d2841e2219
Getaneh48 <getaneh11@gmail.com>
2023-01-24 16:03:14 +0300

    Task 5

--- sort.h
+++ sort.h
@@ -29,4 +29,7 @@ int partition(int *L, int left, int right, size_t size);
 void perform_sort(int *array, size_t low, size_t high);
 void sort(int *L, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 #endif
